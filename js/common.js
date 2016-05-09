344fc439263dafd0388473d3c5f3c87cf3e11f9a
hxpeng <3251335943@qq.com>
2016-05-09 18:04:37 +0800

    2016-5-9  下午六点 增加了几个js插件

--- js/common.js
+++ js/common.js
@@ -5,15 +5,27 @@
 
 
 
-jQuery(function () {
+$(function () {
+
+    var comtentScrollTop;
+
+    $(window).load(function() {
+        //保存滚动条位置，刷新时回到刷新之前的位置
+        comtentScrollTop = $.cookie("comtentScrollTop");
+        if (comtentScrollTop != null) {
+            $("#st-content").scrollTop(comtentScrollTop);
+        }
+    });
 
 
 
     $("#st-content").scroll(function () {
-        $("#st-content").scrollTop() > 1500 ? $("#backTop_box").fadeIn() : $("#backTop_box").fadeOut()
+        comtentScrollTop = $("#st-content").scrollTop();
+        $.cookie("comtentScrollTop", comtentScrollTop, {path: "/"});
+        comtentScrollTop > 1500 ? $("#backTop_box").fadeIn() : $("#backTop_box").fadeOut();
     })
     $("#backTop_box").click(function () {
-        $("#st-content").animate({ scrollTop: 0 }, 800)
+        $("#st-content").animate({scrollTop: 0}, 800)
     })
 
 })
