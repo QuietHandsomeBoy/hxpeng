25af7c696b8f5a5ec3e7827427bf92cd2575bbde
hxpeng <3251335943@qq.com>
2016-07-10 00:11:30 +0800

    2016-7-10  上午     pjax+后台首页

--- js/main.js
+++ js/main.js
@@ -5,12 +5,11 @@
 
 $(function () {
 
-    $('.aniview').AniView();
-    $(window).resize(function(){
+    $(window).resize(function () {
         var leftPX = ($(window).width() - $('#loginBox').outerWidth()) / 2;
         var topPX = ($(window).height() - $('#loginBox').outerHeight()) / 2 + $(document).scrollTop();
 
-        if(topPX < 70){
+        if (topPX < 70) {
             topPX = 70;
         }
         $('#loginBox').css({
@@ -21,30 +20,34 @@ $(function () {
     });
     $(window).resize();
 
-//        $("#test").click(function(){
-//            $(".loadingbox").addClass("loadingbox_test");
-//        })
-    $(document).pjax("a", "#loginContentBox")
-        .on("pjax:timeout", function(event) {
+    $(function(){
+
+    })
+
+    $(document).pjax("a", "#leftBox", {fragment:'#leftBox'})
+        .on("pjax:timeout", function (event) {
             event.preventDefault()
         })
-        .on("pjax:start", function() {
+        .on("pjax:start", function () {
             $(".loadingbox").addClass("loadingbox_test");
         })
-        .on("pjax:send", function() {
+        .on("pjax:send", function () {
             NProgress.start();
             $("#loginContent").fadeTo(500, 0);
         })
-        .on("pjax:success", function() {
-            alert("嘻嘻");
-            setTimeout(function(){NProgress.done();$(".loadingbox").removeClass("loadingbox_test");},500);
+        .on("pjax:success", function () {
+
+            setTimeout(function () {
+                NProgress.done();
+            }, 500);
         })
-        .on("pjax:end", function() {
-            alert("嘻嘻");
-            setTimeout(function(){NProgress.done();$(".loadingbox").removeClass("loadingbox_test");},5000);
+        .on("pjax:popstate",function(){
         })
-//                .on("click", ".forgatPasswordHref", function(e){
-//                    alert("test");
-//                })
+        .on("pjax:end",function(){
+            $('#leftBox .aniview').AniView();
+        })
+        .on("pjax:beforeReplace",function(){
+        })
+
 
 })
